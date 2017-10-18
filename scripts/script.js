1183f806b6b41cfa8494e65262b279f380e5cc77
Matthew Conrad <matthew.conrad@outlook.com>
2017-10-18 13:41:38 -0700

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 $(() => {
 
   // Titles and iMac slider effects
-  $( '#txt-col' ).show( { effect: 'slide', direction: 'up', duration: 1000 } );
-  $( '#imac' ).show( { effect: 'slide', direction: 'left', duration: 1000 } );
+  $( '#txt-col' ).show( { effect: 'slide', direction: 'left', duration: 1000 } );
+  $( '#imac' ).show( { effect: 'slide', direction: 'down', duration: 1000 } );
 
-  // Adds shadow to nav bar on scroll
+  // Adds .small class to nav bar on scroll
   // For mobile / tablet
   $( document ).on( 'scroll', function() {
   	if ( $( document ).scrollTop() > 100 ) {
-  		$( "#main-nav" ).removeClass( 'large' ).addClass( 'small' );
+  		$( '#main-nav' ).removeClass( 'large' ).addClass( 'small' );
     } else {
-  		$( "#main-nav" ).removeClass( 'small' ).addClass( 'large' );
+  		$( '#main-nav' ).removeClass( 'small' ).addClass( 'large' );
   	}
   });
   // For desktop
   $( document ).on( 'scroll', function() {
-  	if ( $( document ).scrollTop() > 100 ) {
-  		$( "#nav-lg" ).removeClass( 'large' ).addClass( 'small' );
+  	if ( $( document ).scrollTop() > 50 ) {
+  		$( '#nav-lg' ).removeClass( 'large' ).addClass( 'small' );
   	} else {
-  		$( "#nav-lg" ).removeClass( 'small' ).addClass( 'large' );
+  		$( '#nav-lg' ).removeClass( 'small' ).addClass( 'large' );
   	}
   });
 
@@ -44,7 +44,7 @@ $(() => {
     }
   });
   $( document ).on( 'scroll', function() {
-    if ( $( document ).scrollTop() > 2200 ) {
+    if ( $( document ).scrollTop() > 2300 ) {
       $( '#portfolio .section-header' ).show( 'puff' );
     }
   });
@@ -63,25 +63,25 @@ $(() => {
   // Nav bar links
   $( '#first' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-1' ).first().offset().top )
+      scrollTop: ( $( '#services' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#second' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-2' ).first().offset().top )
+      scrollTop: ( $( '#about' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#third' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-3' ).first().offset().top )
+      scrollTop: ( $( '#portfolio' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#fourth' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-4' ).first().offset().top )
+      scrollTop: ( $( '#contact' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
@@ -89,25 +89,25 @@ $(() => {
   // Nav bar links for desktop
   $( '#first-lg' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-1' ).first().offset().top )
+      scrollTop: ( $( '#services' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#second-lg' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-2' ).first().offset().top )
+      scrollTop: ( $( '#about' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#third-lg' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-3' ).first().offset().top )
+      scrollTop: ( $( '#portfolio' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
   $( '#fourth-lg' ).on( 'click', (e) => {
     $( 'html, body' ).animate({
-      scrollTop: ( $( '#page-4' ).first().offset().top )
+      scrollTop: ( $( '#contact' ).first().offset().top )
     }, 1000 );
     $( '.hamburger' ).toggleClass( 'is-active' );
   });
