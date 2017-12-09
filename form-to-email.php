edbec53772f056fe8392d3e2b7858d8c7fcd5c12
Matthew Conrad <matthew.conrad@outlook.com>
2017-12-09 13:25:05 -0800

    Changed form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,24 +1,23 @@
 <?php
+
 if(!isset($_POST["submit"]))
 {
-	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
 $name = $_POST["name"];
 $visitor_email = $_POST["email"];
 $message = $_POST["message"];
 
-//Validate first
 if(empty($name)||empty($visitor_email))
 {
-    echo "Name and email are mandatory!";
-    exit;
+  echo "Name and email are mandatory!";
+  exit;
 }
 
 if(IsInjected($visitor_email))
 {
-    echo "Bad email value!";
-    exit;
+  echo "Bad email value!";
+  exit;
 }
 
 $email_from = "mattconrad@gator4128.hostgator.com";
@@ -28,32 +27,10 @@ $email_body = "From: $name\n\n".
 	"Visitor Email: ".
 $headers .= "$visitor_email \n";
 $to = "matthew.conrad@outlook.com";
-//Send the email.
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header("Location: thank-you.html");
 
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: thank-you.html");
 }
 
 ?>
