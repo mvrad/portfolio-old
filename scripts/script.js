3d520784bfaa241e970cc74077da5944dfcc275a
Matthew Conrad <matthew.conrad@outlook.com>
2017-12-11 17:28:07 -0800

    Minor changes to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,128 +7,102 @@ $(() => {
   Landing page opening titles and effects
   ============================================================================*/
   // Titles and slider effects
-  $('#txt-col').show({effect: 'slide', direction: 'down', duration: 1000});
-  $('#slider-wrapper').show({ effect: 'fade', duration: 1000 });
+  $("#txt-col").show({effect: "slide", direction: "down", duration: 1000});
+  $("#slider-wrapper").show({ effect: "fade", duration: 1000 });
 
   /*============================================================================
   Adds .small class to nav bar on scroll
   ============================================================================*/
   // For mobile / tablet
-  $(document).on('scroll', () => {
+  $(document).on("scroll", () => {
   	if ($(document).scrollTop() > 100) {
-  		$('#main-nav').removeClass('large').addClass('small');
+  		$("#main-nav").removeClass("large").addClass("small");
     } else {
-  		$('#main-nav').removeClass('small').addClass('large');
+  		$("#main-nav").removeClass("small").addClass("large");
   	}
   });
   // For desktop
-  $(document).on('scroll', () => {
+  $(document).on("scroll", () => {
   	if ($(document).scrollTop() > 50) {
-  		$('#nav-lg').removeClass('large').addClass('small');
+  		$("#nav-lg").removeClass("large").addClass("small");
   	} else {
-  		$('#nav-lg').removeClass('small').addClass('large');
+  		$("#nav-lg").removeClass("small").addClass("large");
   	}
   });
 
   /*============================================================================
   Title effects
   ============================================================================*/
-  $(document).on('scroll', () => {
+  $(document).on("scroll", () => {
     if ($(document).scrollTop() > 400) {
-      $('#services .section-header').show('fade');
+      $("#services .section-header").show("fade");
     }
     if ($(document).scrollTop() > 700) {
-      $.each($('.skill-wrapper'), (i, el) => {
+      $.each($(".skill-wrapper"), (i, el) => {
         setTimeout(() => {
-          $(el).show({effect: 'drop', direction: 'down', duration: 1000});
+          $(el).show({effect: "drop", direction: "down", duration: 1000});
         }, (i * 250));
       });
     }
     if ($(document).scrollTop() > 1400) {
-      $('#about .section-header').show('fade');
+      $("#about .section-header").show("fade");
     }
     if ($(document).scrollTop() > 1700) {
-      $('#about p').show('fade');
+      $("#about p").show("fade");
     }
     if ($(document).scrollTop() > 2200) {
-      $('#portfolio .section-header').show('fade');
-      $('#portfolio-filter').show('fade');
+      $("#portfolio .section-header").show("fade");
+      $("#portfolio-filter").show("fade");
     }
     if ($( document).scrollTop() > 2400) {
-      $('.grid').show({effect: 'drop', direction: 'down', duration: 1000});
+      $(".grid").show({effect: "drop", direction: "down", duration: 1000});
     }
   });
 
   /*============================================================================
   Hamburger menu animation
   ============================================================================*/
-  $('.hamburger').on('click', () => {
-    $('.hamburger').toggleClass('is-active');
-    $('#site-nav').toggle({effect: 'slide', direction: 'up', duration: 500});
-    $(this).toggleClass('nav');
+  $(".hamburger").on("click", () => {
+    $(".hamburger").toggleClass("is-active");
+    $("#site-nav").toggle({effect: "slide", direction: "up", duration: 500});
+    $(this).toggleClass("nav");
   });
-  $('.nav a').on('click', () => {
-    $('#site-nav').hide();
+  $(".nav a").on("click", () => {
+    $("#site-nav").hide();
   });
 
   /*============================================================================
   Nav bar links
   ============================================================================*/
-  $('#first').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#services').first().offset().top)
+  $("#first, #first-lg").on("click", () => {
+    $("html, body").animate({
+      scrollTop: ($("#services").first().offset().top)
     }, 1000);
-    $('.hamburger').toggleClass('is-active');
+    $(".hamburger").toggleClass("is-active");
   });
-  $('#second').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#about').first().offset().top)
+  $("#second, #second-lg").on("click", () => {
+    $("html, body").animate({
+      scrollTop: ($("#about").first().offset().top)
     }, 1000);
-    $('.hamburger').toggleClass('is-active');
+    $(".hamburger").toggleClass("is-active");
   });
-  $('#third').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#portfolio').first().offset().top)
+  $("#third, #third-lg").on("click", () => {
+    $("html, body").animate({
+      scrollTop: ($("#portfolio").first().offset().top)
     }, 1000);
-    $('.hamburger').toggleClass('is-active');
+    $(".hamburger").toggleClass("is-active");
   });
-  $('#fourth').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#contact').first().offset().top)
+  $("#fourth, #fourth-lg").on("click", () => {
+    $("html, body").animate({
+      scrollTop: ($("#contact").first().offset().top)
     }, 1000);
-    $('.hamburger').toggleClass('is-active');
-  });
-
-  // Nav bar links for desktop
-  $('#first-lg').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#services').first().offset().top)
-    }, 1000);
-    $('.hamburger').toggleClass('is-active');
-  });
-  $('#second-lg').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#about').first().offset().top)
-    }, 1000);
-    $('.hamburger').toggleClass('is-active');
-  });
-  $('#third-lg').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#portfolio').first().offset().top)
-    }, 1000);
-    $('.hamburger').toggleClass('is-active');
-  });
-  $('#fourth-lg').on('click', () => {
-    $('html, body').animate({
-      scrollTop: ($('#contact').first().offset().top)
-    }, 1000);
-    $('.hamburger').toggleClass('is-active');
+    $(".hamburger").toggleClass("is-active");
   });
 
   /*============================================================================
   Photo slider
   ============================================================================*/
-  $('#macbook-slider').slick({
+  $("#macbook-slider").slick({
     // Disable defaults
     accessibility: false,
     draggable: false,
@@ -141,28 +115,28 @@ $(() => {
     arrows: false,
 
     autoplay: true,
-    autoplaySpeed: 2000,
+    infinite: true,
     fade: true,
     speed: 1000,
-    infinite: true,
+    autoplaySpeed: 2000
   });
 
   /*============================================================================
   Sequential Filtering Component
   ============================================================================*/
-  const $filters = $('.filter [data-filter]'),
-        $boxes = $('.boxes [data-category]');
+  const $filters = $(".filter [data-filter]"),
+        $boxes = $(".boxes [data-category]");
 
-  $filters.on('click', function(e) {
+  $filters.on("click", function(e) {
     e.preventDefault();
     const $this = $(this);
 
-    $filters.removeClass('active');
-    $this.addClass('active');
+    $filters.removeClass("active");
+    $this.addClass("active");
 
-    const $filterType = $this.attr('data-filter');
+    const $filterType = $this.attr("data-filter");
 
-    if ($filterType == 'all') {
+    if ($filterType == "all") {
       $boxes.fadeOut().promise().done(() => {
         $boxes.fadeIn();
       });
@@ -176,13 +150,13 @@ $(() => {
   /*============================================================================
   Keep Filter Links Highlighted
   ============================================================================*/
-  $('a').click(() => {
-    $(this).toggleClass('active');
+  $("a").click(() => {
+    $(this).toggleClass("active");
   });
 
   /*============================================================================
   Dynamic copyright year
   ============================================================================*/
-  $('#year').html(new Date().getFullYear());
+  $("#year").html(new Date().getFullYear());
 
 });
