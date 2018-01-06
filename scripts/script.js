863cb0f362ba04c323df6c785a33e9d0236d55fd
Matthew Conrad <matthew.conrad@outlook.com>
2018-01-06 13:22:48 -0800

    Added mailer.php and added AJAX validation

--- scripts/script.js
+++ scripts/script.js
@@ -136,7 +136,35 @@ $(() => {
   $("a").click(() => {
     $(this).toggleClass("active");
   });
+  // Contact Email Form
+  const form = $("#ajax-contact");
+  const formMessages = $("#form-messages");
+  $(form).submit(function(e) {
+    e.preventDefault();
+    const formData = $(form).serialize();
+    $.ajax({
+      type: "POST",
+      url: $(form).attr("action"),
+      data: formData
+    })
+    .done(function(response) {
+      $(formMessages).removeClass("error");
+      $(formMessages).addClass("success");
+      $(formMessages).text(response);
+      $("#name").val("");
+      $("#email").val("");
+      $("#message").val("");
+    })
+    .fail(function(data) {
+      $(formMessages).removeClass("success");
+      $(formMessages).addClass("error");
+      if (data.responseText !== "") {
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).text("Oops! An error occured and your message could not be sent.");
+      }
+    });
+  });
   // Dynamic copyright year
   $("#year").html(new Date().getFullYear());
-
 });
