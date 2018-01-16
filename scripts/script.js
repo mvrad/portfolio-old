0af0581ecf46115ff6b6b5d10522b5aa2639f928
Matthew Conrad <matthew.conrad@outlook.com>
2018-01-15 16:07:59 -0800

    Removed .travis.yml

--- scripts/script.js
+++ scripts/script.js
@@ -170,10 +170,4 @@ $(() => {
   });
   // Dynamic copyright year
   $("#year").html(new Date().getFullYear());
-  // Travis CI server test
-  let http = require("http");
-  http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end();
-  }).listen(1337, "127.0.0.1");
 });
